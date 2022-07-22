b64c7b09467cea2f0f81da46e05b053dbd0c8f18
irg1008 <ivansudevlop@gmail.com>
2022-07-22 13:08:41 +0200

    Updated optimizer and scheduler for more natural language proccess

--- scripts/visualize_model.py
+++ scripts/visualize_model.py
@@ -4,7 +4,18 @@ import typer
 
 def main(models: str, default_text: str):
     models = [name.strip() for name in models.split(",")]
-    spacy_streamlit.visualize(models, default_text, visualizers=["ner"])
+    default_text = default_text[1:-1]
+    visualizers = ["ner"]
+
+    spacy_streamlit.visualize(
+        models,
+        default_text,
+        visualizers=visualizers,
+        show_visualizer_select=False,
+        show_logo=False,
+        color="red",
+        sidebar_title="Yoda NER"
+    )
 
 
 if __name__ == "__main__":
