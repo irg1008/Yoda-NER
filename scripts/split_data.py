8bc2921f838d8cea87b2d41ba098bb6826d49af1
irg1008 <ivansudevlop@gmail.com>
2022-07-25 13:20:04 +0200

    Added duplicity capacity to split script

--- scripts/split_data.py
+++ scripts/split_data.py
@@ -1,3 +1,4 @@
+from unittest.mock import CallableMixin
 import typer
 import pandas as pd
 import json
@@ -5,7 +6,7 @@ from pathlib import Path
 from random import shuffle
 from split_types import Entity, Entities, TitleData, TitlesData
 import re
-from typing import Union
+from typing import Union, Callable
 
 # IMPORTANT: First column must be title column. Rest can be any N features wanted.
 
@@ -75,10 +76,35 @@ def export_json(titles_data: TitlesData, json_path: Path):
         f.write(json.dumps(titles_data))
 
 
+def duplicate_data(data: pd.DataFrame) -> pd.DataFrame:
+    """
+    Duplicate data with uppercase, capitalize and lowercase title.
+    Then concat all those in a single dataframe.
+
+    Args:
+        data (pd.DataFrame): Data to duplicate.
+
+    Returns:
+        pd.DataFrame: Duplicated data
+    """
+
+    def copy(data: pd.DataFrame, str_op: Callable):
+        data_copy = data.copy()
+        data_copy.title = data_copy.title.apply(str_op)
+        return data_copy
+
+    data_upper = copy(data, str_op=str.upper)
+    data_capitalize = copy(data, str_op=str.capitalize)
+    data_lower = copy(data, str_op=str.lower)
+
+    return pd.concat([data, data_upper, data_capitalize, data_lower])
+
+
 def main(csv_path: Path, train_path: Path, val_path: Path, train_split: float):
     data = read_csv(csv_path)
-    titles_data = get_titles_data(data)
 
+    duplicated_data = duplicate_data(data)
+    titles_data = get_titles_data(duplicated_data)
     train_data, val_data = split(titles_data, train_size=train_split)
 
     export_json(train_data, train_path)
