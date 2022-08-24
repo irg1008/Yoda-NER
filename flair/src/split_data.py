e182c7178c33d70a3162406b291a390cff2720e8
irg1008 <ivansudevlop@gmail.com>
2022-08-24 13:36:56 +0200

    Added data duplication before split

--- flair/src/split_data.py
+++ flair/src/split_data.py
@@ -1,3 +1,4 @@
+from typing import Callable
 import pandas as pd
 from os import path
 
@@ -25,11 +26,34 @@ def export_csv(data: pd.DataFrame, csv_path: str):
     data.to_csv(csv_path, index=False)
 
 
+def duplicate_data(data: pd.DataFrame) -> pd.DataFrame:
+    """
+    Duplicate data with uppercase, capitalize and lowercase title.
+    Then concat all those in a single dataframe.
+    Args:
+        data (pd.DataFrame): Data to duplicate.
+    Returns:
+        pd.DataFrame: Duplicated data
+    """
+
+    def copy(data: pd.DataFrame, str_op: Callable):
+        data_copy = data.copy()
+        data_copy.title = data_copy.title.apply(str_op)
+        return data_copy
+
+    data_upper = copy(data, str_op=str.upper)
+    data_capitalize = copy(data, str_op=str.capitalize)
+    data_lower = copy(data, str_op=str.lower)
+
+    return pd.concat([data, data_upper, data_capitalize, data_lower])
+
+
 def main():
     data_folder = path.join(path.dirname(__file__), "../data/")
     splits_folder = data_folder + "splits/"
 
     data = pd.read_csv(data_folder + "features.csv")
+    data = duplicate_data(data)
 
     train_data, val_data, test_data = split(data, TRAIN, VAL, 1.0 - TRAIN - VAL)
 
